d23e0b8ba544390439888bb0faf5d7faf4d997f2
Matthew Egbedokun <jimatth121@gmail.com>
2023-04-19 20:12:36 +0100

    Side Menu

--- src/SlideMenu.js
+++ src/SlideMenu.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './SlideMenu.css'
+import { Link } from 'react-router-dom'
 
 const SlideMenu = (props) => {
   return (
     <div className={`slidemenu  ${props.onState ? 'slide__true':''}`}>
+     <Link to="/">
       <span>Home</span>
+     </Link>
+     <Link to="/testing">
       <span>Products</span>
+     </Link>
       <button>Login</button>
     </div>
   )
